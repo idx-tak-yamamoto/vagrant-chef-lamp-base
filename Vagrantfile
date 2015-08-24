9cbe91deb0cb7a261506c1e829ef571bd4988a03
idx-tak-yamamoto <tak-yamamoto@indexweb.co.jp>
2015-08-24 16:14:04 +0900

    first commit

---
+++ Vagrantfile
@@ -0,0 +1,23 @@
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "opscode-centos-6.5"
+  config.vm.hostname = "centos"
+  config.vm.box_url = "http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_centos-6.5_chef-provisionerless.box"
+
+  # host(127.0.0.1)の8080番を、guest(仮想環境)の80番にポートフォワードする
+  config.vm.network :forwarded_port, guest: 80, host: 8080
+  config.vm.network :forwarded_port, guest: 443, host: 443
+  # Chef
+  config.vm.provision "chef_solo" do |chef|
+    chef.cookbooks_path = ["./cookbooks"]
+    chef.add_recipe "webapp"
+  end
+
+   # 仮想環境にインストールするchefのバージョン
+  config.omnibus.chef_version = :latest
+
+  config.vm.synced_folder "D:\\tmp\\vagrant\\webapp\\www", "/home/www"
+
+end
+
---
+++ cookbooks/webapp/recipes/default.rb
@@ -0,0 +1,63 @@
+# packegaのインストール
+#  sudo yum install httpd
+package "httpd" do
+  action :install
+end
+
+package "httpd-devel" do
+  action :install
+end
+
+package "mod_ssl" do
+  action :install
+end
+
+# 起動と自動起動の設定
+#  sudo service httpd start
+#  sudo chkconfig on
+service "httpd" do
+  action [:enable]
+  supports :status => true, :restart => true, :reload => true
+end
+
+
+# conf の配置
+template "virtualhost.conf" do
+  path "/etc/httpd/conf.d/virtualhost.conf"
+  source "virtualhost.conf.erb"
+  mode 0644
+  notifies :start, 'service[httpd]' 
+  notifies :reload, 'service[httpd]'
+end
+
+
+package "php" do
+  action :install
+end
+
+package "php-devel" do
+  action :install
+end
+
+package "php-mysql" do
+  action :install
+end
+
+package "mysql" do
+  action :install
+end
+
+package "mysql-server" do
+  action :install
+end
+
+service "mysqld" do
+  action [ :enable, :start ]
+end
+
+#mysql root pass設定
+execute "set root password" do
+  command "mysqladmin -u root password ''"
+  only_if "mysql -u root -e 'show databases;'"
+end
+
